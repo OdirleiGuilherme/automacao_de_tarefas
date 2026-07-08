de5fc67bcd1ea021449bf20153343dba63dc294d
Odirlei Guilherme <odirlei.guilherme11@gmail.com>
2026-07-07 23:10:31 -0300

    feat>: add arquivo captura_position.py

---
+++ captura_position.py
@@ -0,0 +1,5 @@
+import pyautogui
+from time import sleep
+
+sleep(5)  # Aguarda 5 segundos
+print(pyautogui.position())
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from time import sleep
 pyautogui.PAUSE = 1.5
 
 link = 'https://dlp.hashtagtreinamentos.com/python/intensivao/login'
+email = 'odirlei_007@hotmail.com'
+password = '12345678'
 
 pyautogui.press('win')  # Pressiona a tecla Windows
 pyautogui.write('edge')  # Digita "Edge" para abrir o navegador
@@ -14,4 +16,10 @@ pyautogui.press('enter')  # Pressiona Enter para acessar o site
 
 sleep(3)  # Aguarda 3 segundos para o site carregar
 
-pyautogui.click()
+pyautogui.click(1751, 367)  # Clica no campo de login
+pyautogui.write(email)  # Digita o nome de usuário
+
+pyautogui.press('tab')  # Pressiona a tecla Tab para ir para o campo de senha
+
+pyautogui.write(password)  # Digita a senha
+pyautogui.press('enter')  # Pressiona Enter para fazer login
